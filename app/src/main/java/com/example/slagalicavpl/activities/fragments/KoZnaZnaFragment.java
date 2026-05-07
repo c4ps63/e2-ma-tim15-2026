9081d1b59d75a3875e307b887c3eed3108e0f8e4
c4ps63 <pavlekapuran1@gmail.com>
2026-05-07 22:40:36 +0100

    KoZnaZna fragment

---
+++ app/src/main/java/com/example/slagalicavpl/activities/fragments/KoZnaZnaFragment.java
@@ -0,0 +1,18 @@
+package com.example.slagalicavpl.activities.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.slagalicavpl.R;
+
+public class KoZnaZnaFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ko_zna_zna, container, false);
+    }
+}
